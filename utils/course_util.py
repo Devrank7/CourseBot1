7d491376953052e45133a2ed0751c012b66af60e
Levlink773 <cleandev9@gmail.com>
2025-05-24 15:29:13 +0300

    com 8

--- utils/course_util.py
+++ utils/course_util.py
@@ -1,4 +1,4 @@
-course_data1 = {
+course_data = {
     "name": "קורסי לידה🤰",
     "modules": [
         {
@@ -14,8 +14,181 @@ course_data1 = {
             ]
         },
         {
-            "name": "קורס הכנה ללידה",
+            "name": "פרטי: קורס האב התומך המושלם",
             "id": 1,
+            "lections": [
+                {
+                    "name": "שיעור 1 - נוכחות מלווה הלידה",
+                    "id": 0,
+                    "path": "videos/module2/l1.mp4",
+                    "file_id": "BAACAgIAAxkBAAMhaC3rRwKBkP51-kn4IiKQcWaA4QYAAtd3AALbfXBJ8kXsM2M9DSI2BA"
+                },
+                {
+                    "name": "שיעור 2 - המלצות לזמן היריון",
+                    "id": 1,
+                    "path": "videos/module2/l2.mp4",
+                    "file_id": "BAACAgIAAxkBAAM9aC3wyd1j1qRynRkTkIKMBYyvSlwAAjh4AALbfXBJUBxh4zdt3MY2BA"
+                },
+                {
+                    "name": "שיעור 3 - המלצות בזמן הלידה",
+                    "id": 2,
+                    "path": "videos/module2/l3.mp4",
+                    "file_id": "BAACAgIAAxkBAAM-aC3wyf_fxAABkD0wVDKWyRrO63L8AAI6eAAC231wSQNJTTmyt6yXNgQ"
+                },
+                {
+                    "name": "שיעור 4 - התנהגות נכונה אחרי לידה",
+                    "id": 3,
+                    "path": "videos/module2/l4.mp4",
+                    "file_id": "BAACAgIAAxkBAAM_aC3wyXhR40GaHlyefDfxdXTEnGYAAjx4AALbfXBJ3ijYWdgH7HI2BA"
+                },
+                {
+                    "name": "שיעור 5 - שמירה על תשוקה לאחר הלידה",
+                    "id": 4,
+                    "path": "videos/module2/l5.mp4",
+                    "file_id": "BAACAgIAAxkBAANAaC3wyTk0-POTUHMqB_xe-_YWht0AAj14AALbfXBJ-wtCAAH9nc-1NgQ"
+                },
+                {
+                    "name": "שיעור 2 - מה זה פרינאום",
+                    "id": 5,
+                    "path": "videos/private/2.mp4",
+                    "file_id": "BAACAgIAAxkBAAICVWgxt1M-hC7niVGZWx_JwYKJbEBfAAJdcgACuLqRSaEijlFNnf6iNgQ"
+                },
+                {
+                    "name": "שיעור 4 - שיעורי בית",
+                    "id": 6,
+                    "path": "videos/private/4.mp4",
+                    "file_id": "BAACAgIAAxkBAAICV2gxt1NqyNWWrWDW3lFxkm7UENszAAJmcgACuLqRSd-VJnYGoEuXNgQ"
+                },
+                {
+                    "name": "שיעור 5 - איך להתכופף נכון",
+                    "id": 7,
+                    "path": "videos/private/5.mp4",
+                    "file_id": "BAACAgIAAxkBAAICWGgxt1OOMgkKAAEiaDyA4WFhcKy59QACaHIAAri6kUkJct-F0ub26DYE"
+                },
+                {
+                    "name": "שיעור 3 - חמישה תנאים אנטומיים ללידה",
+                    "id": 8,
+                    "path": "videos/private/3.mp4",
+                    "file_id": "BAACAgIAAxkBAAICVmgxt1Ocb0RYikZZdG5AnTeIN26aAAJhcgACuLqRSZMTd6POuo5qNgQ"
+                },
+            ]
+        },
+        {
+            "name": "קורס הכנה ללידה עם צלקת על הרחם",
+            "id": 2,
+            "lections": [
+                {
+                    "name": "הקדמה של לידה עם צלקת על הרחם",
+                    "id": 0,
+                    "path": "videos/module2/1.mp4",
+                    "file_id": "BAACAgIAAxkBAAM7aC3wWs7z-Lg9KLqrRF3L1PgrStgAAi94AALbfXBJNgfvcvvmbf42BA"
+                },
+                {
+                    "name": "שיעור 1 - איך לנשום נכון",
+                    "id": 1,
+                    "path": "videos/module1/l1.mp4",
+                    "file_id": "BAACAgIAAxkBAAP9aC4PpNM52AwTETzFgD1k5qbW-fIAAl96AALbfXBJ866RJQPXJ_Y2BA"
+                },
+                {
+                    "name": "שיעור 2 - מה זה פרינאום",
+                    "id": 2,
+                    "path": "videos/module1/l2.mp4",
+                    "file_id": "BAACAgIAAxkBAAMlaC3uaRuB4FnFe4q-0ahuYx9dV6UAAu53AALbfXBJjWvzehco3fQ2BA"
+                },
+                {
+                    "name": "שיעור 3 - חמישה תנאים אנטומיים ללידה",
+                    "id": 3,
+                    "path": "videos/module1/l3.mp4",
+                    "file_id": "BAACAgIAAxkBAAMmaC3uaef6nrgoy9ElLE4SRIp_U8YAAvd3AALbfXBJ-DF4ZmkgEQY2BA"
+                },
+                {
+                    "name": "שיעור 4 - שיעורי בית",
+                    "id": 4,
+                    "path": "videos/module1/l4.mp4",
+                    "file_id": "BAACAgIAAxkBAAMnaC3uaXjyFBPR8-6wCVbmtZbI1VgAAvx3AALbfXBJ-bq54RlTGPg2BA"
+                },
+                {
+                    "name": "שיעור 5 - איך להתכופף נכון",
+                    "id": 5,
+                    "path": "videos/module1/l5.mp4",
+                    "file_id": "BAACAgIAAxkBAAMoaC3uaXhPqYxYNSzCx6casY2gB58AAgZ4AALbfXBJVsNMXOUZE102BA"
+                },
+                {
+                    "name": "שיעור 6 - שלבי לידה",
+                    "id": 6,
+                    "path": "videos/module1/l6.mp4",
+                    "file_id": "BAACAgIAAxkBAAMpaC3uaQrWPBZCsEng_XYhn7ITvScAAgh4AALbfXBJzWdEzCQg24U2BA"
+                },
+                {
+                    "name": "שיעור 7 - התנהגות נכונה בלידה",
+                    "id": 7,
+                    "path": "videos/module1/l7.mp4",
+                    "file_id": "BAACAgIAAxkBAAMqaC3uae29bx_TK_9_ByA_pbDIMN4AAgl4AALbfXBJgEgeoUU2d5I2BA"
+                },
+                {
+                    "name": "שיעור 8 - המלצות למלווה של היולדת",
+                    "id": 8,
+                    "path": "videos/module1/l8.mp4",
+                    "file_id": "BAACAgIAAxkBAAMraC3uab43Y6S6K4btjKmcin0EUA4AAgt4AALbfXBJN-Zi6dZi5MQ2BA"
+                },
+                {
+                    "name": "שיעור 9 - איך לעשות דמיון מודרך ללידה",
+                    "id": 9, "path": "videos/module1/l9.mp4",
+                    "file_id": "BAACAgIAAxkBAAMSaC3P8c64okC5E0HNwKbr1_XKEXcAAo11AALbfXBJCFOpMpgAAS13NgQ"
+                },
+                {
+                    "name": "שיעור 10 - תיק ללידה",
+                    "id": 10,
+                    "path": "videos/module1/l10.mp4",
+                    "file_id": "BAACAgIAAxkBAAMtaC3uafhe4MY2IcdWgiACryzvumQAAg54AALbfXBJKnuP612ZOFM2BA"
+                },
+                {
+                    "name": "שיעור 11 - חדר לידה",
+                    "id": 11,
+                    "path": "videos/module1/l11.mp4",
+                    "file_id": "BAACAgIAAxkBAAMuaC3uaVAS_513zZC6COWjzD8Bi_IAAhJ4AALbfXBJD2rFJxgfoIU2BA"
+                },
+                {
+                    "name": "שיעור 12 - אפידורל",
+                    "id": 12,
+                    "path": "videos/module1/l12.mp4",
+                    "file_id": "BAACAgIAAxkBAAM5aC3wA-DKiS87H-yWULMBIw5mb2wAAiZ4AALbfXBJoRZVnyXXZ6g2BA"
+                },
+                {
+                    "name": "שיעור 1 - איך לנשום נכון",
+                    "id": 13,
+                    "path": "videos/private/1.mp4",
+                    "file_id": "BAACAgIAAxkBAAICVGgxt1PeowdI3P_avgAB9Eee_F2p5gACWnIAAri6kUnIwKIhjaKeATYE"
+                },
+                {
+                    "name": "שיעור 2 - מה זה פרינאום",
+                    "id": 14,
+                    "path": "videos/private/2.mp4",
+                    "file_id": "BAACAgIAAxkBAAICVWgxt1M-hC7niVGZWx_JwYKJbEBfAAJdcgACuLqRSaEijlFNnf6iNgQ"
+                },
+                {
+                    "name": "שיעור 3 - חמישה תנאים אנטומיים ללידה",
+                    "id": 15,
+                    "path": "videos/private/3.mp4",
+                    "file_id": "BAACAgIAAxkBAAICVmgxt1Ocb0RYikZZdG5AnTeIN26aAAJhcgACuLqRSZMTd6POuo5qNgQ"
+                },
+                {
+                    "name": "שיעור 4 - שיעורי בית",
+                    "id": 16,
+                    "path": "videos/private/4.mp4",
+                    "file_id": "BAACAgIAAxkBAAICV2gxt1NqyNWWrWDW3lFxkm7UENszAAJmcgACuLqRSd-VJnYGoEuXNgQ"
+                },
+                {
+                    "name": "שיעור 5 - איך להתכופף נכון",
+                    "id": 17,
+                    "path": "videos/private/5.mp4",
+                    "file_id": "BAACAgIAAxkBAAICWGgxt1OOMgkKAAEiaDyA4WFhcKy59QACaHIAAri6kUkJct-F0ub26DYE"
+                },
+            ]
+        },
+        {
+            "name": "קורס הכנה ללידה",
+            "id": 3,
             "lections": [
                 {
                     "name": "הקדמה של קורס הכנה ללידה",
@@ -94,65 +267,11 @@ course_data1 = {
                     "path": "videos/module1/l12.mp4",
                     "file_id": "BAACAgIAAxkBAAM5aC3wA-DKiS87H-yWULMBIw5mb2wAAiZ4AALbfXBJoRZVnyXXZ6g2BA"
                 },
-                {
-                    "name": "קורס הכנה ללידה - פרסומת",
-                    "id": 13,
-                    "path": "videos/module2/l6.mp4",
-                    "file_id": "BAACAgIAAxkBAAIBo2guN5gIp3L6oNQ57FdCw5NgAAHJcwAC1XwAAtt9cElhi4P5MH4emzYE"
-                },
-            ]
-        },
-        {
-            "name": "קורס הכנה ללידה עם צלקת על הרחם",
-            "id": 2,
-            "lections": [
-                {
-                    "name": "הקדמה של לידה עם צלקת על הרחם",
-                    "id": 0,
-                    "path": "videos/module2/1.mp4",
-                    "file_id": "BAACAgIAAxkBAAM7aC3wWs7z-Lg9KLqrRF3L1PgrStgAAi94AALbfXBJNgfvcvvmbf42BA"
-                },
-                {
-                    "name": "שיעור 1 - נוכחות מלווה הלידה",
-                    "id": 1,
-                    "path": "videos/module2/l1.mp4",
-                    "file_id": "BAACAgIAAxkBAAMhaC3rRwKBkP51-kn4IiKQcWaA4QYAAtd3AALbfXBJ8kXsM2M9DSI2BA"
-                },
-                {
-                    "name": "שיעור 2 - המלצות לזמן היריון",
-                    "id": 2,
-                    "path": "videos/module2/l2.mp4",
-                    "file_id": "BAACAgIAAxkBAAM9aC3wyd1j1qRynRkTkIKMBYyvSlwAAjh4AALbfXBJUBxh4zdt3MY2BA"
-                },
-                {
-                    "name": "שיעור 3 - המלצות בזמן הלידה",
-                    "id": 3,
-                    "path": "videos/module2/l3.mp4",
-                    "file_id": "BAACAgIAAxkBAAM-aC3wyf_fxAABkD0wVDKWyRrO63L8AAI6eAAC231wSQNJTTmyt6yXNgQ"
-                },
-                {
-                    "name": "שיעור 4 - התנהגות נכונה אחרי לידה",
-                    "id": 4,
-                    "path": "videos/module2/l4.mp4",
-                    "file_id": "BAACAgIAAxkBAAM_aC3wyXhR40GaHlyefDfxdXTEnGYAAjx4AALbfXBJ3ijYWdgH7HI2BA"
-                },
-                {
-                    "name": "שיעור 5 - שמירה על תשוקה לאחר הלידה",
-                    "id": 5,
-                    "path": "videos/module2/l5.mp4",
-                    "file_id": "BAACAgIAAxkBAANAaC3wyTk0-POTUHMqB_xe-_YWht0AAj14AALbfXBJ-wtCAAH9nc-1NgQ"
-                },
-                {
-                    "name": "פרסומת לקורס - לידה עם טפר על הרחם",
-                    "id": 6,
-                    "path": "videos/module2/l6.mp4",
-                    "file_id": "BAACAgIAAxkBAAIBoWguNo266Lq8kcunte2syFq2mWqHAALGfAAC231wSSfzndwq6Mh1NgQ"
-                },
             ]
         },
     ]
 }
-course_data = {
+course_data1 = {
     "name": "קורסי לידה🤰",
     "modules": [
         {
